0c4d978d4b1b4a198c520a281666ab22fb418bd2
Adugna <support@evangadi.com>
2020-02-09 16:19:51 -0500

    Update index.php
    Test change

--- RhodeIsland_Liquors/www.rhodeislandliquors.dev.cc/index.php
+++ RhodeIsland_Liquors/www.rhodeislandliquors.dev.cc/index.php
@@ -3,8 +3,9 @@
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * @package WordPress
+ * @package WordPress Test 
  */
+
 
 /**
  * Tells WordPress to load the WordPress theme and output it.
